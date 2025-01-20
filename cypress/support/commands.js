f0f5ee17cb2bb43f943c8e0d507ad06aad0f82e6
Mariane-br <mariane.0306@outlook.com>
2025-01-20 13:14:17 -0300

    Adicionado comentários ao comando customizado de login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,15 @@
+// Adiciona um comando customizado no Cypress chamado 'login'
 Cypress.Commands.add('login', () => {
-    cy.intercept('GET', '**').as('allRequests')
+    // Intercepta todas as requisições GET feitas pela aplicação e cria um alias chamado 'allRequests'
+    cy.intercept('GET', '**').as('allRequests');
 
-    cy.get('#user-name').type('standard_user')
-    cy.get('#password').type('secret_sauce')
-    cy.get('#login-button').click()
+    // Localiza os campos de usuário e senha, insere os valores e faz o login
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
 
-    cy.wait('@allRequests').its('response.statusCode').should('eq', 200)
-})
+    // Aguarda que todas as requisições interceptadas sejam concluídas
+    cy.wait('@allRequests')
+        // Verifica que o código de status da resposta da requisição interceptada é 200 (sucesso)
+        .its('response.statusCode').should('eq', 200);
+});
