80234bf71aecc1586e46af3e9972373826935b2b
Mariane-br <mariane.0306@outlook.com>
2025-01-20 13:10:57 -0300

    Criação de comando customizado para o login com credenciais válidas, ajuste no código de login respeitando as boas práticas e criado cenário para teste de login com dados inválidos

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,31 +1,49 @@
+// Lista de usuários para os testes, com combinações de credenciais (válidas e inválidas)
+const pessoas = [{
+        // Usuário inválido e senha válida
+        "usuario": "joao",
+        "senha": "secret_sauce"
+    },
+    {
+        // Usuário válido e senha inválida
+        "usuario": "standard_user",
+        "senha": "123entrar"
+    },
+    {
+        // Usuário e senha inválidos
+        "usuario": "ana",
+        "senha": "master123"
+    }
+]
+
 describe('login', () => {
 
+    // Visita a URL definida na variável de ambiente 'baseUrl' no arquivo de configuração do Cypress antes de cada teste dentro deste bloco
     beforeEach(() => {
-        cy.visit('https://www.saucedemo.com/')
-    })
+        cy.visit(Cypress.env('baseUrl'));
+    });
 
+    // Teste para login com dados válidos
     it('login com dados válidos', () => {
-        cy.intercept('GET', '**').as('allRequests')
+        // Comando customizado que realiza o login com credenciais válidas
+        cy.login();
+    });
 
-        cy.get('#user-name').type('standard_user')
-        cy.get('#password').type('secret_sauce')
-        cy.get('#login-button').click()
+    // Teste para login com dados inválidos
+    it('Login com dados inválidos', () => {
+        // Itera sobre o array 'pessoas', realizando o teste para cada combinação de usuário e senha
+        pessoas.forEach(pessoa => {
+            // Limpa o campo do usuário antes de inserir um novo valor e insere o nome de usuário da iteração atual
+            cy.get('#user-name').clear().type(pessoa.usuario);
 
-        cy.wait('@allRequests').its('response.statusCode').should('eq', 200)
-    })
+            // Limpa o campo da senha antes de inserir um novo valor e insere a senha da iteração atual
+            cy.get('#password').clear().type(pessoa.senha);
 
-    it.only('Login com usuário inválido e senha válida', () => {
-        cy.get('#user-name').type('mariane')
-        cy.get('#password').type('secret_sauce')
-        cy.get('#login-button').click()
-        cy.get('h3[data-test="error"]').should('be.visible')
-    })
+            // Clica no botão de login
+            cy.get('#login-button').click();
 
-    it('Login com usuário válido e senha inválida', () => {
-
-    })
-
-    it('Login com usuário e senha inválidos', () => {
-
-    })
-})
+            // Verifica se a mensagem de erro é exibida após o envio de credenciais inválidas
+            cy.get('h3[data-test="error"]').should('be.visible');
+        });
+    });
+});
